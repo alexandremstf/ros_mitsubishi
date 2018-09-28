9d5682b1752e83a3332cdd6ee4c7ff35a9f0f873
alexandre <alexandremstf@gmail.com>
2018-09-28 10:52:58 -0300

    Primeira comunicação funcional

--- Serial.py
+++ Serial.py
@@ -1,16 +1,16 @@
 import serial
 
-communnication = serial.Serial(port = '/dev/ttyUSB0', baudrate = 9600)
+serialCom = serial.Serial(port = 'COM1', 
+baudrate = 9600,
+parity = serial.PARITY_EVEN, 
+stopbits = serial.STOPBITS_ONE, 
+bytesize = serial.EIGHTBITS)
 
-communnication.write("1;1;OPEN=NARCUSR")
-communnication.write("1;1;PARRLNG")
-communnication.write("1;1;PDIRTOP")
-communnication.write("1;1;PPOSF")
-communnication.write("1;1;PARMEXTL")
-communnication.write("1;1;KEYWDptest")
-communnication.write("1;1;SRVON")
-communnication.write("1;1;RSTALRM")
-communnication.write("1;1;STATE")
-communnication.write("1;1;CNTLON")
+#print serialCom
 
-communnication.close()
+serialCom.write(('1;1;OPEN=NARCUSR'+'\r').encode())
+serialCom.write(('1;1;CNTLON'+'\r').encode())
+serialCom.write(('1;1;SRVON'+'\r').encode())
+serialCom.write(('1;1;CNTLOFF'+'\r').encode())
+
+serialCom.close()
