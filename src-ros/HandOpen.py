cd10de6696183ff989c8a309cc9a89eb99d3d192
Paulo Santos <paulosantosphs@hotmail.com>
2018-12-08 15:01:28 -0200

    hand pacotes

---
+++ src-ros/HandOpen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+path = os.getcwd() + '/src/ros_mitsubishi/src'
+
+import sys
+sys.path.insert(0, path)
+
+import RobotArm
+robot = RobotArm.RobotArm()
+
+import rospy
+
+def listener():
+    robot.init()
+
+    rospy.init_node('talker', anonymous=False)
+    robot.handOpen()
+
+    robot.turnOff()
+
+if __name__ == '__main__':
+    listener()
