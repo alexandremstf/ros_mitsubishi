917cbab635fbe40ce97996e37af6b18678260c9d
Paulo Santos <paulosantosphs@hotmail.com>
2018-12-08 14:01:18 -0200

    path

--- src-ros/JointMovementListener.py
+++ src-ros/JointMovementListener.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import os
+path = os.getcwd() + '/src/ros_mitsubishi/src'
 
 import sys
-sys.path.insert(0, '/home/paulo/catkin_ws/src/ros_mitsubishi/src')
+sys.path.insert(0, path)
 
 import RobotArm
 robot = RobotArm.RobotArm()
